93c0711deded38245d02bd152bb5f4ff0e468e34
Jose <jose.zdy@gmail.com>
2019-01-27 18:59:36 -0500

    add sensor pins contants

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,13 @@
 import os
 
+
+MAVLINK_ADDRESS = "udp:127.0.0.1:14551"
+
+
+################################################################################
+#### Directory contants
+################################################################################
+
 SRC_DIR = os.path.dirname(os.path.realpath(__file__))
 BASE_DIR = os.path.dirname(SRC_DIR)
 
@@ -7,4 +15,19 @@ DATA_DIR = "%s/data" % BASE_DIR
 LOGS_DIR = "%s/logs" % BASE_DIR
 
 
-MAVLINK_ADDRESS = "udp:127.0.0.1:14551"
+################################################################################
+#### External hardware pins
+################################################################################
+
+# AM2302 Temperature, Humidity sensor
+AM2302_PINS = {
+    'out': 4,
+}
+
+# BMP180 Temperature, Pressure sensor (SPI Interface)
+BMP180_PINS = {
+    'cs': 22,
+    'mosi': 10,
+    'miso': 9,
+    'clk': 11,
+}
