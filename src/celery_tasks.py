9ac12d63e3e26976d368bdc870e3e0df3bdf2ade
Jose <jose.zdy@gmail.com>
2019-02-10 18:15:34 -0500

    fix typo

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -11,13 +11,13 @@ app = Celery('tasks', broker=settings.CELERY_BROKER)
 redis_queue = redis.Redis(**settings.REDIS_CONF)
 
 @app.task
-def am3202_measure(sensor, pin):
+def am2302_measure(sensor, pin):
     humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     data = {
         'ts': time.time(),
         'humidity': humidity,
         'temperature': temperature,
     }
-    redis_queue.set('AM3202_data', json.dumps(data))
+    redis_queue.set('AM2302_data', json.dumps(data))
 
 
