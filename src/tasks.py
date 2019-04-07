c685b5e8c41daf41852a7a058564ec243691cde0
Jose <jose.zdy@gmail.com>
2019-04-07 16:23:41 -0500

    take_pic and extract_faces methods implementation

--- src/tasks.py
+++ src/tasks.py
@@ -1,8 +1,25 @@
-from datetime import datetime
+import time
+import settings
 
-from sensors.sensor_tasks import am2302_measure
+
+def am2302_measure():
+    import Adafruit_DHT
+    humidity, temperature = Adafruit_DHT.read_retry(
+        Adafruit_DHT.AM2302,
+        settings.AM2302_PINS['out']
+    )
+    # from random import random
+    # humidity, temperature = 90 + 3 * random(), 17 + 3 * random()
+    data = {
+        'ts': time.time(),
+        'humidity': float("{:.2f}".format(humidity)),
+        'temperature': float("{:.2f}".format(temperature)),
+    }
+    return data
+
 
 def collect_data(position):
+    from datetime import datetime
 
     # this is a syncronous job that blocks normal flow
     # TODO: run sensor mesasures async, then get the last sensed value here
@@ -18,3 +35,41 @@ def collect_data(position):
         'am2302': am2302_data,
     }
     return new_data
+
+
+
+
+
+def take_pic():
+    from picamera import PiCamera
+
+    pic_name = "{}.jpg".format(time.strftime("%Y%m%d_%H%M%S"))
+    pic_path = "{}/{}".format(settings.PICS_DIR, pic_name)
+
+    with PiCamera() as cam:
+        cam.rotation = 180
+        cam.capture(pic_path)
+
+    return pic_path
+
+
+def extract_faces(pic_path):
+    import os
+    import face_recognition
+    from PIL import Image
+
+    image = face_recognition.load_image_file(pic_path)
+    face_locations = face_recognition.face_locations(image)
+    index = 0
+
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        face_image = image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+
+        face_path = "{}/face_{}.jpg".format(settings.FACES_DIR, index)
+        while os.path.isfile(face_path):
+            index += 1
+            face_path = "{}/face_{}.jpg".format(settings.FACES_DIR, index)
+
+        pil_image.save(face_path)
