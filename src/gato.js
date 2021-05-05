27b204d26f2b4e14adaa7908fecb3564576ee37a
Jose B <jose.zdy@gmail.com>
2021-05-05 00:39:06 -0500

    better gato ws handling

--- src/gato.js
+++ src/gato.js
@@ -11,14 +11,37 @@ const PROD = TUKANO_ENV === "PROD"
 const IKARO_ENDPOINT = PROD ? `wss://icaro.tucanorobotics.co` : `ws://localhost:8000`
 const WS_IKARO = `${IKARO_ENDPOINT}/mavlink/plate/${PLATE}`
 
-const ws_ikaro = new WebSocket(WS_IKARO)
+let ws_ikaro
 const ws_tukano = new WebSocket.Server({ port: 8080 })
 
 ws_tukano.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        ws_ikaro.send(message)
+    ws.on('message', function (message) {
+        if (ws_ikaro.readyState === WebSocket.OPEN) ws_ikaro.send(message)
     })
-    ws_ikaro.on('message', function incoming(message) {
+    ws_ikaro.on('message', function (message) {
         ws.send(message)
     })
+    ws.on('error', function (e) {
+        console.error("WS WS ERROR:", e)
+    })
 })
+
+ws_tukano.on('error', function (e) {
+    console.error("WS TUKANO ERROR:", e)
+})
+
+function connect_ikaro() {
+    ws_ikaro = new WebSocket(WS_IKARO)
+
+    ws_ikaro.on('close', function (e) {
+        console.log('IKARO ws is closed. Reconnecting in 1 second:', e)
+        setTimeout(function () { connect_ikaro() }, 1000)
+    })
+
+    ws_ikaro.on('error', function (e) {
+        console.error('IKARO ws error: ', e)
+        ws_ikaro.close()
+    })
+}
+
+connect_ikaro()
