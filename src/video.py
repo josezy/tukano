aee7ae353653a1f4b20340a0d8f5301235042cb7
Jose B <jose.zdy@gmail.com>
2021-06-09 16:35:02 -0500

    down framerate

--- src/video.py
+++ src/video.py
@@ -9,7 +9,7 @@ import picamera
 
 camera = picamera.PiCamera()
 camera.resolution = (320, 240)
-camera.framerate = 30
+camera.framerate = 20
 # Start a preview and let the camera warm up for 2 seconds
 camera.start_preview()
 time.sleep(2)
